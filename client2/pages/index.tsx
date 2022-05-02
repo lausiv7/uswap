1c17ded2dafc6f6f96490ba81980e3260c7e31e7
lausiv7 <lausiv@gmail.com>
2022-05-03 06:37:40 +0900

    third

---
+++ client2/pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import Header from '../components/Header'
+
+const style = {
+  wrapper: `h-screen max-h-screen h-min-screen w-screen bg-[#2D242F] text-white select-none flex flex-col justify-between`,
+}
+
+export default function Home(){
+  return (
+    <div className={style.wrapper}>
+      <Header /> 
+      <h2>Main 2</h2>
+      <h2>TransactionHistory</h2>
+    </div>
+
+  )
+}
